64ef2436bbda9addd272c1de15ae50a7ff243f6a
Adel Bassiony <a.bassiony@icloud.com>
2020-09-24 14:36:53 +0200

    Added: Loader Component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = props => {
+    return (
+        <div className="container">
+            <div className="row align-items-center" style={{minHeight: '800px'}}>
+                <div className="col text-center">
+                    <h1><strong>About You</strong></h1>
+                    <p className="mt-3">Loading...</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Loader;
